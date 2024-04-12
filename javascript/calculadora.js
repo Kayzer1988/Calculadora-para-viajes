a03e9efb352f8aad454f93df46641edcc51c9fcd
Alejandro Escobar <jorge.alejandro.escobar.170@gmail.com>
2024-04-11 23:48:30 -0300

    segundo commit

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -1,18 +1,89 @@
-/* //Es una calculadora para una remiseria que quier averiguar el valor del viaje y sus impuestos en caso de requerirlos. */
+/* //Es una calculadora para una remiseria que quiere averiguar el valor del viaje y sus impuestos en caso de requerirlos. */
+
+class clientesNuevos{
+    constructor (nombre,direccion,telefono,clasificacion){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.clasificacion = clasificacion;
+    }
+}
+
+const clientesRemiseria = [
+    {
+        nombre: "Daiana Alderete",
+        direccion: "Belgrano 1259",
+        telefono: 1189105641,
+        clasificacion: "VIP"
+    },
+    {
+        nombre: "Ariel Kubar",
+        direccion: "Fatima 550",
+        telefono: 1152857998,
+        clasificacion: "VIP"
+    },
+    {
+        nombre: "Rufina Silvero",
+        direccion: "Av. San Martin 250",
+        telefono: 1156327866,
+        clasificacion: "Basico"
+    },
+    {
+        nombre: "Martin Acosta",
+        direccion: "Croacia 3560",
+        telefono: 1155698525,
+        clasificacion: "Basico"
+    }
+
+];
+
+console.log(clientesRemiseria)
+
+const clasiFiltrado = clientesRemiseria.filter(cliente => cliente.clasificacion === "VIP");
+
+let clienteElegido
+let esperaEnElViaje;
+let espera = 0;
+let valorDeLaEspera;
+let totalViaje;
+let descuentoVip
+let descuentoVipFactura
+
+while (true){
+
+    let elegirOpcion = parseInt(prompt("Ingrese el numero de la opción deseada:\n 1- Quiere buscar un cliente?\n 2-Agregar nuevo cliente?"));
+
+    if (elegirOpcion == 1){
+    let elegirCliente = prompt("Ingrese el nombre del cliente a buscar.");
+
+    clienteElegido = clientesRemiseria.find(cliente =>cliente.nombre.toLowerCase().includes(elegirCliente.toLowerCase()))
+        break;
+    } else if (elegirOpcion == 2){
+        nombre = prompt ("Ingrese el nombre del nuevo cliente.");
+        direccion = prompt("Ingrese la direccion del cliente nuevo.");
+        telefono = parseInt(prompt("Ingresa el numero de telefono del nuevo cliente"));
+        clasificacion = prompt("Ingrese el tipo de clasificacion de cliente./n Ingrese una de las opciones: VIP o Basico.");
+        const nuevoCliente = new clientesNuevos(nombre,direccion,telefono,clasificacion);
+        clientesRemiseria.push(nuevoCliente);
+        clienteElegido = nuevoCliente;
+        break;
+    }  else {
+        alert("Código incorrecto. Por favor, ingrese un codigo valido.?");
+    }
+}
+
+console.log(clienteElegido);
 
 //Variables
 let kmDelViaje = parseInt(prompt("Ingresa la cantidad de km del viaje."));
 
 let valorDelKm = parseInt(prompt("Ingresa el valor del km para este viaje"));
 
-let esperaEnElViaje;
-let espera = 0;
-let valorDeLaEspera;
-let totalViaje;
+
 
 //Bucle para saber si el cliente necesita o no espera.
 while (true) {
-    esperaEnElViaje = parseInt(prompt("¿El cliente necesita que se lo espere?\nIngrese el código correspondiente:\n1-Si\n2-No"));
+    esperaEnElViaje = parseInt(prompt("¿El cliente " + clienteElegido.nombre + " necesita que se lo espere?\nIngrese el código correspondiente:\n1-Si\n2-No"));
 
     if (esperaEnElViaje == 1) {
         espera += 1;
@@ -37,20 +108,35 @@ function espera0(kmDelViaje, valorDelKm) {
     let totalViaje = kmDelViaje * valorDelKm;
 
     while (true) {
-        let factura = parseInt(prompt("¿El cliente requiere factura?\nIngrese el código correspondiente:\n1-Si\n2-No"));
+        let factura = parseInt(prompt("¿" + clienteElegido.nombre + " requiere factura?\nIngrese el código correspondiente:\n1-Si\n2-No"));
 
         if (factura == 1) {
             let Total = totalViaje * 1.21;
-            alert("El precio del viaje final con factura es $" + Total);
-            break; // Salir del bucle
+
+            if (clasiFiltrado.find(cliente => cliente.nombre === clienteElegido.nombre)){
+                descuentoVipFactura = Total * 0.90;
+                alert("🎉Felicidades🎉 " + clienteElegido.nombre + "Como cliente VIP, hemos aplicado un descuento especial a su tarifa. El precio final del viaje es aún más bajo de lo que esperaba, el total a pagar con factura es $" + descuentoVipFactura);
+                break;           
+            } else {
+                alert(clienteElegido.nombre + " el precio del viaje final con factura es $" + Total);
+                break; // Salir del bucle
+            }
         } else if (factura == 2) {
-            alert("Gracias por viajar con nosotros, el total del viaje es $" + totalViaje);
-            break; // Salir del bucle
+
+            if (clasiFiltrado.find(cliente => cliente.nombre === clienteElegido.nombre)){
+                descuentoVip = totalViaje * 0.90;
+                alert("🎉Felicidades🎉 " + clienteElegido.nombre + "Como cliente VIP, hemos aplicado un descuento especial a su tarifa. El precio final del viaje es aún más bajo de lo que esperaba, el total a pagar es $" + descuentoVip);
+                break;
+            } else {
+                alert("Gracias " + clienteElegido.nombre + " por viajar con nosotros, el total del viaje es $" + totalViaje);
+                break; // Salir del bucle
+            }
         } else {
             alert("Código incorrecto. Por favor, ingrese 1 o 2."); //repite bucle.
         }
     }
 }
+
 
 //Calculo del valor del viaje con espera.
 function espera1(kmDelViaje, valorDelKm) {
@@ -59,17 +145,32 @@ function espera1(kmDelViaje, valorDelKm) {
 
 
     while (true) {
-        let factura = parseInt(prompt("¿El cliente requiere factura?\nIngrese el código correspondiente:\n1-Si\n2-No"));
+        let factura = parseInt(prompt("¿" + clienteElegido.nombre + " requiere factura?\nIngrese el código correspondiente:\n1-Si\n2-No"));
 
         if (factura == 1) {
             let Total = totalViajeConEspera * 1.21;
-            alert("El precio del viaje final con factura es $" + Total);
-            break; // Salir del bucle
+
+            if (clasiFiltrado.find(cliente => cliente.nombre === clienteElegido.nombre)){
+                let descuentoVipFactura = Total * 0.90;
+                alert("🎉Felicidades🎉 " + clienteElegido.nombre + "Como cliente VIP, hemos aplicado un descuento especial a su tarifa. El precio final del viaje es aún más bajo de lo que esperaba, el total a pagar con factura es $" + descuentoVipFactura);
+                break;
+            } else {
+                alert(clienteElegido.nombre + " el precio del viaje final con factura es $" + Total);
+                break; // Salir del bucle
+            }
         } else if (factura == 2) {
-            alert("Gracias por viajar con nosotros, el total del viaje es $" + totalViajeConEspera);
-            break; // Salir del bucle
+
+            if (clasiFiltrado.find(cliente => cliente.nombre === clienteElegido.nombre)){
+                let descuentoVip = totalViajeConEspera * 0.90;
+                alert("🎉Felicidades🎉 " + clienteElegido.nombre + "Como cliente VIP, hemos aplicado un descuento especial a su tarifa. El precio final del viaje es aún más bajo de lo que esperaba, el total a pagar es $" + descuentoVip);
+                break;
+            } else {
+                alert("Gracias " + clienteElegido.nombre + " por viajar con nosotros, el total del viaje es $" + totalViajeConEspera);
+                break; // Salir del bucle
+            }
         } else {
             alert("Código incorrecto. Por favor, ingrese 1 o 2."); //repite bucle.
         }
     }
 }
+
